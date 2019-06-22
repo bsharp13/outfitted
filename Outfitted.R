8daaaf93cbb27b4528caf2904451df8afdd6d11d
Braden Sharp <bsharp@qualtrics.com>
2019-06-22 12:19:41 -0600

    Routine update

---
+++ Outfitted.R
@@ -0,0 +1,42 @@
+
+library(magrittr)
+library(tidyverse)
+library(gmailr)
+
+#-------------------------------------------------------------------------------
+
+# Read in data
+#-------------------------------------------------------------------------------
+shirt <- read_csv('Shirts.csv')
+pants <- read_csv('Pants.csv')
+
+outfit <- shirt %>%  
+  gather(key = 'Pants', value = 'Match', -(ID:Liking)) %>% 
+  filter(Match == 1) %>% 
+  select(-Match) %>% 
+  left_join(pants, by = c('Pants' = 'Description'), suffix = c('_S', '_P')) %>% 
+  filter(!is.na(ID_P))
+
+#-------------------------------------------------------------------------------
+
+# Randomly generate
+#-------------------------------------------------------------------------------
+
+#FIXME: Pull in weather
+
+outfit %>% 
+  mutate(
+    #DaysSince = today() - LastWorn_S,
+    Priority = 1
+  ) %>% 
+  arrange(sample(1:n()), desc(Priority)) %>% 
+  head(7) %>% 
+  select(Description, Pants)
+
+#-------------------------------------------------------------------------------
+
+# Send email
+#-------------------------------------------------------------------------------
+
+
+
